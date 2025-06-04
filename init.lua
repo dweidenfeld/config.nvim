0743da9420f0930cbcba0f4ecf2429d1758a8f5f
Dominik Weidenfeld <dominik@weidenfeld.io>
2025-06-04 17:54:52 +0200

    added copilot

--- init.lua
+++ init.lua
@@ -662,6 +662,8 @@ require('lazy').setup({
     },
   },
 
+  'github/copilot.vim',
+
   { -- You can easily change to a different colorscheme.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
